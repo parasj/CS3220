54e88d7b078b8cca1193236e3983b755e5affca1
nliu96 <theaznnoobie@gmail.com>
2016-10-06 18:32:55 -0400

    Update TestClockMultiplier.v

--- p1/TestClockMultiplier.v
+++ p1/TestClockMultiplier.v
@@ -1,3 +1,17 @@
-module TestClockMultipler;
-	
-endmodule
+`timescale 1ns / 1ps
+module TestClockMultipler();
+    reg     reset;
+    reg     clk_in;
+    wire    clk_out;
+	wire[3:0] num_out;
+	ClockMultiplier clk_mult(reset, clk_in, 2, clk_out, num_out);
+
+    always
+        #20 clk_in = !clk_in;
+
+    initial begin
+        reset   =   0;
+        #100
+        $stop;
+    end
+end
