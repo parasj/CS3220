c1ca9240f7f488314e6c50b030ecb2d1fc2cedeb
Paras Jain <parasjain@parasjain.com>
2016-10-31 00:51:06 -0400

    ALU update

--- single_cycle_cpu/ALU.v
+++ single_cycle_cpu/ALU.v
@@ -7,6 +7,15 @@
 `define ALU_NOR 4'b0110
 `define ALU_XNOR 4'b0111
 
+`define ALU_MVHI 4'b1000
+`define ALU_EQ 4'b1001
+`define ALU_LT 4'b1010
+`define ALU_LTE 4'b1011
+`define ALU_T 4'b1100
+`define ALU_NE 4'b1101
+`define ALU_GTE 4'b1110
+`define ALU_GT 4'b1111
+
 `define CMD_EQ 2'b00
 `define CMD_LT 2'b01
 `define CMD_GT 2'b10
@@ -14,32 +23,120 @@
 
 
 module ALU(aluop, a, b, c, cmdflag);
-	parameter WIDTH = 8;
-
+	parameter WIDTH = 32;
 
 	input[3:0] aluop;
-	input[WIDTH-1:0] a;
-	input[WIDTH-1:0] b;
-	output[WIDTH-1:0] c;
+	input[WIDTH - 1:0] a;
+	input[WIDTH - 1:0] b;
+	output reg[WIDTH-1:0] c;
+	output reg cmdflag;
 
-	// 00 = 0
-	// 01 = lt 0
-	// 10 = gt 0
-	// 11 = unused
-	output[1:0] cmdflag;
+	wire[WIDTH - 1:0] out_one;
+	assign out_one = {{(WIDTH - 1){1'b0}}, 1'b1};
+	wire[WIDTH - 1:0] out_zero;
+	assign out_zero = {{(WIDTH - 1){1'b0}}, 1'b0};
 
-	assign c = (aluop == `ALU_ADD) ? (a + b) :
-			   (aluop == `ALU_SUB) ? (a - b) :
-			   (aluop == `ALU_AND) ? (a & b) :
-			   (aluop == `ALU_OR) ? (a | b) :
-			   (aluop == `ALU_XOR) ? (a ^ b) :
-			   (aluop == `ALU_NAND) ? ~(a & b) :
-			   (aluop == `ALU_NOR) ? ~(a | b) :
-			   (aluop == `ALU_XNOR) ? a ^~ b :
-			   {WIDTH{1'bx}};
 
-	assign cmdflag = (c == {WIDTH{1'b0}}) ? `CMD_EQ :
-					 (c[WIDTH-1] == 1'b1) ? `CMD_LT :
-					 (c[WIDTH-1] == 1'b0) ? `CMD_GT :
-					 `CMD_XX;
+	always @(aluop or a or b) begin
+		case (aluop)
+			`ALU_ADD: begin
+				c <= a + b;
+				cmdflag <= 1'b0;
+			end
+			`ALU_SUB: begin
+				c <= a - b;
+				cmdflag <= 1'b0;
+			end
+			`ALU_AND: begin
+				c <= a & b;
+				cmdflag <= 1'b0;
+			end
+			`ALU_OR: begin
+				c <= a | b;
+				cmdflag <= 1'b0;
+			end
+			`ALU_XOR: begin
+				c <= a ^ b;
+				cmdflag <= 1'b0;
+			end
+			`ALU_NAND: begin
+				c <= ~(a & b);
+				cmdflag <= 1'b0;
+			end
+			`ALU_NOR: begin
+				c <= ~(a | b);
+				cmdflag <= 1'b0;
+			end
+			`ALU_XNOR: begin
+				c <= a ^~ b;
+				cmdflag <= 1'b0;
+			end
+			`ALU_MVHI: begin
+				c <= ((b & (1 << ((WIDTH >> 1) + 1))) << (WIDTH >> 1));
+				cmdflag <= 1'b0;
+			end
+			`ALU_EQ: begin
+				if (a == b) begin
+					c <= out_one;
+					cmdflag <= 1'b1;
+				end else begin
+					c <= out_zero;
+					cmdflag <= 1'b0;
+				end
+			end
+			`ALU_LT: begin
+				if (a < b) begin
+					c <= out_one;
+					cmdflag <= 1'b1;
+				end else begin
+					c <= out_zero;
+					cmdflag <= 1'b0;
+				end
+			end
+			`ALU_LTE: begin
+				if (a <= b) begin
+					c <= out_one;
+					cmdflag <= 1'b1;
+				end else begin
+					c <= out_zero;
+					cmdflag <= 1'b0;
+				end
+			end
+			`ALU_T: begin
+				c <= out_one;
+				cmdflag <= 1'b1;
+			end
+			`ALU_NE: begin
+				if (a != b) begin
+					c <= out_one;
+					cmdflag <= 1'b1;
+				end else begin
+					c <= out_zero;
+					cmdflag <= 1'b0;
+				end
+			end
+			`ALU_GTE: begin
+				if (a >= b) begin
+					c <= out_one;
+					cmdflag <= 1'b1;
+				end else begin
+					c <= out_zero;
+					cmdflag <= 1'b0;
+				end
+			end
+			`ALU_GT: begin
+				if (a > b) begin
+					c <= out_one;
+					cmdflag <= 1'b1;
+				end else begin
+					c <= out_zero;
+					cmdflag <= 1'b0;
+				end
+			end
+			default: begin
+				c <= {WIDTH{1'bx}};
+				cmdflag <= 1'b0;
+			end
+		endcase
+	end
 endmodule
