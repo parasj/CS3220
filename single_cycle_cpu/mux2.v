5dccd5d29e369c888067be6fd3a336136749caa9
Paras Jain <parasjain@parasjain.com>
2016-11-01 14:40:09 -0400

    Inline assign

--- single_cycle_cpu/mux2.v
+++ single_cycle_cpu/mux2.v
@@ -1,10 +1,5 @@
 module Mux2(s, d0, d1, out);
 	input s;
 	input [31 : 0] d0, d1;
-	output [31 : 0] out;
-
-	wire [31 : 0] mux_out;
-
-	assign out = mux_out;
-	assign mux_out = s ? d1 : d0;
+	output [31 : 0] out = s ? d1 : d0;
 endmodule
